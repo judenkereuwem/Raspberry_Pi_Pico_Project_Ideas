a0bce4efddcd0862bb151015d84b203195ea029f
unknown <judenkereuwem@gmail.com>
2023-06-22 09:08:30 +0100

    cyclone game

---
+++ pico led cyclone game/pico_cyclone_game.py
@@ -0,0 +1,38 @@
+from machine import Pin, PWM
+import neopixel
+import time
+import random
+
+button = Pin(15, Pin.IN, Pin.PULL_DOWN)
+buzzer = PWM(Pin(16))
+buzzer.freq(500)
+
+np = neopixel.NeoPixel(machine.Pin(17), 16)
+
+target = random.randint(0,15)
+speed = round(random.uniform(0.03, 0.05),2)
+print(speed)
+    
+while True:
+    
+    for i in range(0, 16):
+        print(button.value())
+        np[i] = (255,0,0)
+        np[target] = (0,255,0)
+        np.write()
+        time.sleep(speed)
+        np[i] = (0,0,0)
+        
+        while button.value() == 1 and i == target:
+            buzzer.duty_u16(1000)
+            time.sleep(0.5)
+            buzzer.duty_u16(0)
+            time.sleep(0.5)
+            print("boom")
+        
+            target = random.randint(0,15)
+
+
+
+
+       
